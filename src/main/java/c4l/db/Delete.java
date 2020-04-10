4507b1b7a50af34b35d27de0e40058d6f7d86e73
medem100 <modellbauer555@web.de>
2020-04-10 22:18:13 +0200

    add Functions to Delete
    scene
    chase
    effectsFromDeviceState
    chaseHasScene

---
+++ src/main/java/c4l/db/Delete.java
@@ -0,0 +1,98 @@
+package c4l.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+public class Delete {
+    private Connection conn;
+    private Logger log;
+
+    protected Delete(Connection conn) {
+        this.conn = conn;
+        log = Logger.getLogger(Delete.class.getName());
+    }
+
+    /**
+     * delete an scene from the setup
+     *
+     * @param id id of the scene
+     */
+    public void scene(int id) {
+        String SQLDeleteSetupHasScene = "delete from setup_has_scene where scene_id=" + id + ";";
+        String SQLDeleteChaseHasScene = "delete from chase_has_scene where scene_id=" + id + ";";
+
+        // TODO delete effect and device statis
+
+        try {
+            conn = dbCreate.getInstance();
+            Statement query = conn.createStatement();
+            //	query.executeUpdate(SQLDeleteSceneSelf);
+            query.executeUpdate(SQLDeleteSetupHasScene);
+            query.executeUpdate(SQLDeleteChaseHasScene);
+
+        } catch (SQLException e) {
+            log.severe("Fail to Delete Scene " + e.toString());
+        }
+
+    }
+
+
+    /**
+     * delete a chase from setup
+     *
+     * @param id      id of the chase
+     * @param setupid setup id
+     */
+    public void chase(int id, int setupid) {
+        String SQLDeleteSetupHasChase = "delete from setup_has_chase where setUp_id=" + setupid + "and case_id=" + id + ";";
+
+        // TODO delete effect and device statis
+
+        try {
+            conn = dbCreate.getInstance();
+            Statement query = conn.createStatement();
+            query.executeUpdate(SQLDeleteSetupHasChase);
+
+        } catch (SQLException e) {
+            log.severe("Fail to Delete Chase : " + e.toString());
+        }
+
+    }
+
+    /**
+     * Delete the effects From the Device Status
+     * @param dsid device State id
+     */
+    protected void effectsFromDeviceState(int dsid) {
+        String SQL = "delete from effect_status where device_status_id = " + dsid;
+        Statement query = null;
+        try {
+            query = conn.createStatement();
+            query.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            log.severe("Fail to Delete Effect from status id : " + throwables.toString());
+        }
+    }
+
+
+    /**
+     * delete all scenes from an chase
+     *
+     * @param chaseId id of the chase
+     */
+    protected void chaseHasScene(int chaseId) {
+
+        String SQLDeleteChaseHasScene = "delete from chase_has_scene where case_id=" + chaseId + ";";
+
+        try {
+            Statement query = conn.createStatement();
+            query.executeUpdate(SQLDeleteChaseHasScene);
+        } catch (SQLException e) {
+            log.severe("Fail to Delete Chase has Scene :" + e.toString());
+        }
+
+    }
+
+}
