44787bcde5096de05ad4b5821dd78a7ec829e2ab
medem100 <modellbauer555@web.de>
2020-04-10 18:38:58 +0200

    add toSaveString to Utile Class
    Is use in the Update class to save the fader values

--- src/main/java/c4l/db/Util.java
+++ src/main/java/c4l/db/Util.java
@@ -2,5 +2,17 @@ package c4l.db;
 
 public class Util {
 
+    /**
+     * convert an array to Save format of the DB
+     * @param array array to save
+     * @return String with the values of the Array
+     */
+    public static String toSaveString(int[] array) {
+        String result = "";
+        for (int e : array) {
+            result += e + Constants.DELIMITER;
+        }
+        return result.substring(0, result.length() - 1);
+    }
 
 }
